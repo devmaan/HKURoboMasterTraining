4823aede65d7424340603a317bee30da94a98785
zeyu qiu <zeyuqiu@zeyudeMacBook-Air.local>
2019-09-24 15:57:16 +0800

    assignment 1

---
+++ a3.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+  
+ int main ()
+ {
+ int n, i;
+ float sum = 0;
+ float average;
+ puts("please put 10 integers:\n");
+ for (i = 0; i<10; i += 1)
+ {
+ scanf("%d", &n);
+ sum += n;
+ }
+ average = sum/10;
+ printf("%f", average);
+ return 0;
+
+ }
+
