deed655a6857b10e3de4c076c5a0640a6a5ea2e2
Teresa-Hung <55734969+Teresa-Hung@users.noreply.github.com>
2019-09-25 11:53:51 +0800

    Update average.c

--- 1_Lan_C/average.c
+++ 1_Lan_C/average.c
@@ -1,9 +1,27 @@
 /*The file take 10 integers from input*
   and then promote the average of the 10 inputs
  * /
-
-
-int main ()
+#include <stdio.h>
+float average(int number[10])
 {
+	float x=0;
+	int i;
+	for(i=0;i<10;i++)
+	{
+		x+=number[i];
+	}
+	x/=10;
+	return x;
+}
+
+int main()
+{
+	int i,n[10];
+	printf("Enter 10 integers:\n");
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&n[i]);
+	}
+	printf("%f",average(n));
 	return 0;
 }
