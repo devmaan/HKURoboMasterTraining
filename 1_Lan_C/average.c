a0fa187faf24460f1c6a0042858e1bd20cacdafd
Dennis <bouncy.lu@hotmail.com>
2019-09-25 15:11:46 +0800

    format file

--- 1_Lan_C/average.c
+++ 1_Lan_C/average.c
@@ -1,9 +1,22 @@
-/*The file take 10 integers from input*
+/*The file take 10 integers from input
   and then promote the average of the 10 inputs
- * /
+*/
 
+#include <stdio.h>
 
-int main ()
-{
-	return 0;
+float avg(){
+    printf("Enter 10 integers: ");
+    int s = 0;
+    for(int i = 0; i < 10; i++){
+        scanf("%d ", &i);
+        s = s + i;
+    }
+    float avg = s / 10.0;
+    return avg;
+}
+
+int main(){
+    float r = avg();
+    printf("%f", r);
+    return 0;
 }
